656f552f7f90cf25e9e8c925f99e39cbe7127954
lightbullet92 <naa_1992@mail.ru>
2025-07-25 15:09:17 +0400

    commit BLANK site

---
+++ SOURCES/SCRIPTS/PHP/_get_pagename.php
@@ -0,0 +1,15 @@
+<?php
+    $file_array_filename = "SOURCES/CONTENT/MAIN/_menu_files.txt";
+    if(file_exists($file_array_filename))
+    {
+        $file_array = file($file_array_filename);
+        foreach($file_array as $array_item)
+        {
+            $file_pagenames = file(trim($array_item));
+            $current = pathinfo($_SERVER['PHP_SELF'])['basename'];
+            foreach($file_pagenames as $index => $item)
+                if(trim($item) == $current)
+                    echo file_get_contents("SOURCES/CONTENT/MAIN/_company.txt") . ': ' . trim($file_pagenames[$index - 1]) . PHP_EOL;
+        }
+    }
+?>
